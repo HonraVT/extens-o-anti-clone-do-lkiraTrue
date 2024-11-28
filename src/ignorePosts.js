446ba0b61c98e7a177f0b10c11b400e9769e5a05
HonraVT <111161607+HonraVT@users.noreply.github.com>
2024-11-28 14:11:52 -0300

    Update ignorePosts.js

--- src/ignorePosts.js
+++ src/ignorePosts.js
@@ -2,28 +2,23 @@ chrome.storage.sync.get(['data', 'blockedUsers', 'allowedUsers'], (result) => {
   const storageData = new StorageData(result.data || {});
   const blockedUsers = new Set(result.blockedUsers || []);
   const allowedUsers = new Set(result.allowedUsers || []);
-  console.log("kkk", allowedUsers);
-  
-
+  //console.log("allowedUsers: ", allowedUsers);
   processPosts(storageData, blockedUsers, allowedUsers);
   observeNewPosts(storageData, blockedUsers, allowedUsers);
   saveBlockedUsers(blockedUsers);
-  console.log(blockedUsers);
+  //console.log("blockedUsers: ", blockedUsers);
 });
 
 function processPosts(data, blocked, allowed) {
-  const posts = document.querySelectorAll('article.message');
+  const posts = getPosts();
   posts.forEach((post) => {
     handlePost(post, data, blocked, allowed);
   });
 }
 
 function handlePost(post, storage, blocked, allowed) {
-  const userName = post.getAttribute('data-author');
-  const numberOfPosts = parseInt(
-    (post.querySelector('.message-userExtras > dl:nth-child(2) > dd')?.textContent.trim().replace(',', '') || '0'),
-    10
-  );
+  const userName = getAuthorName(post);
+  const numberOfPosts = getNumberOfUserPosts(post)
   if (storage.needIgnoreUser(userName, numberOfPosts) && !allowed.has(userName)) {
     blocked.add(userName)
     post.style.display = 'none'; // Oculta o post indesejado
@@ -33,7 +28,7 @@ function handlePost(post, storage, blocked, allowed) {
 }
 
 function observeNewPosts(storage, blocked, allowed) {
-  const targetNode = document.querySelector('.p-body-pageContent');
+  const targetNode = document.querySelector('.p-body-pageContent'); // Pai dos posts
   if (!targetNode) return;
 
   const config = { childList: true, subtree: true }; // Observa adições no DOM
@@ -54,6 +49,6 @@ function observeNewPosts(storage, blocked, allowed) {
 
 function saveBlockedUsers(blocked) {
   chrome.storage.sync.set({ blockedUsers: Array.from(blocked) }, () => {
-    console.log('Blocked users updated:', blocked);
+    //console.log('Blocked users updated:', blocked);
   });
 }
